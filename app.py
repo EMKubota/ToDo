9cfda16f63e9a819dbfdfc97aec7c9d83ff03eec
EMKubota <153694398+EMKubota@users.noreply.github.com>
2024-02-08 09:59:07 -1000

    2/8 fixing currently

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import os.path
-import json
 
 
 app = Flask(__name__)
